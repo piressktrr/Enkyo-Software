7ae4821b3ea88ddefa095e61cbc6e2895a1aacaa
Pedro Henrique <pedrokinter1@gmail.com>
2026-05-07 10:43:14 -0300

    minimal changes
    adicionei o arquivo certo e deixei o velho como backup

---
+++ enkyo.py
@@ -0,0 +1,528 @@
+import flet as ft   
+
+#aqui são as cores bases, pra nn ter que ficar reescrevento
+
+PRIMARY = "#3E8E41"
+ACCENT = "#E74C6E"
+BG = "#F5EBDD"
+
+def main(page: ft.Page):
+
+    page.title = "ENKYO"
+    page.bgcolor = BG
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"  
+
+    #componentes reutilizaveis (talvez, acho que vai ser util, vou ver ainda se vai ficar grande o codigo)
+
+    def input_field(label, password=False, color=BG):
+        return ft.TextField(
+            label=label,
+            password=password,
+            color=color,
+            border=ft.InputBorder.OUTLINE,
+            border_color="transparent",
+            border_radius=20,
+            filled=True,
+            bgcolor="#EDE6D6",
+            text_size=15,
+            width=260,
+            height=45,
+            content_padding=12,
+            margin=ft.Margin.only(left=20, right=20),
+            
+        )
+    
+    def main_button(text, color=ACCENT, on_click=None):
+        return ft.Button(
+            text,
+            bgcolor=color,
+            color="white",
+            width=200,
+            height=45,
+            on_click=on_click,
+            margin=ft.Margin.only(left=20, right=20),
+            style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=12))
+        )
+    
+    def form_container(content):
+        return ft.Container(
+            content=content,
+            bgcolor="#C7D9C1",
+            border_radius=20,
+            padding=25,
+        )
+
+    #aqui são as pages 
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    
+
+
+
+
+    def go_login(e):
+        page.controls.clear()
+
+        titulo = ft.Text(
+            "ENKYO",
+            size=32,
+            weight=ft.FontWeight.BOLD,
+            color=PRIMARY,
+        )
+
+        subtitulo = ft.Text(
+            "CENTRO DE CONVIVÊNCIA\nDO IDOSO",
+            size=24,
+            text_align=ft.TextAlign.CENTER,
+            color=PRIMARY,
+            weight=ft.FontWeight.W_600,
+        )
+
+        telefone = input_field("Telefone")
+        senha = input_field("Senha", password=True)
+
+        form = ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Text(
+                                    "ENTRAR",
+                                    color="white",
+                                    size=28,
+                                    weight=ft.FontWeight.BOLD,
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                bgcolor=PRIMARY,
+                                padding=10,
+                                border_radius=10,
+                                height=60,
+                                expand=True,
+                                alignment=ft.Alignment.CENTER,
+                            ),
+                        ]
+                    ),
+
+                    ft.Text(
+                        "Telefone",
+                        text_align=ft.TextAlign.LEFT,
+                        color="#4e310a",
+                        size=24,
+                        weight=ft.FontWeight.BOLD,
+                        ),
+                    telefone,
+
+                    ft.Text(
+                        "Senha",
+                        text_align=ft.TextAlign.LEFT,
+                        color="#4e310a",
+                        size=24,
+                        weight=ft.FontWeight.BOLD,
+                        ),
+                    senha,
+
+                    ft.Container(height=12),
+
+                    main_button("CONFIRMAR", on_click=go_dashboard),
+                    main_button("VOLTAR", on_click=home),
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=10,
+            ),
+            bgcolor="#c4dbc1",
+            border_radius=15,
+            padding=ft.padding.only(bottom=20),
+            margin=40,
+            expand=True,
+        )
+
+        layout = ft.Column(
+            [
+                titulo,
+                subtitulo,
+                ft.Container(height=10),
+                form,
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
+
+        page.add(layout)
+        page.update()
+            
+
+
+
+
+
+
+
+    def go_register(e):
+        page.controls.clear()
+
+        titulo = ft.Container(
+            content=ft.Text(
+                "CADASTRO",
+                size=26,
+                weight=ft.FontWeight.BOLD,
+                color="white",
+                text_align=ft.TextAlign.CENTER,
+            ),
+            bgcolor=PRIMARY,
+            padding=15,
+            border_radius=12,
+        )
+
+        nome = input_field("Nome completo")
+        telefone = input_field("Telefone")
+        senha = input_field("Senha", password=True)
+        confirmar = input_field("Confirmar senha", password=True)
+
+        form = ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.Row(
+                        controls=[titulo],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+
+                    ft.Container(height=10),
+
+                    ft.Text(
+                        "Nome completo",
+                        color="#4e310a",
+                        size=20,
+                        weight=ft.FontWeight.W_500,
+                    ),
+                    nome,
+
+                    ft.Text(
+                        "Telefone",
+                        color="#4e310a",
+                        size=20,
+                        weight=ft.FontWeight.W_500,
+                    ),
+                    telefone,
+
+                    ft.Text(
+                        "Senha",
+                        color="#4e310a",
+                        size=20,
+                        weight=ft.FontWeight.W_500,
+                    ),
+                    senha,
+
+                    ft.Text(
+                        "Confirmação da Senha",
+                        color="#4e310a",
+                        size=20,
+                        weight=ft.FontWeight.W_500,
+                    ),
+                    confirmar,
+
+                    ft.Container(height=15),
+
+                    main_button("CONFIRMAR", on_click=go_dashboard),
+                    main_button("VOLTAR", on_click=home),
+                ],
+                spacing=10,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+            bgcolor="#c4dbc1",
+            border_radius=20,
+            padding=20,
+            width=360,
+        )
+
+        page.add(
+            ft.Row(
+                controls=[form],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+        )
+
+        page.update()
+
+
+
+
+
+    def go_dashboard(e):
+        page.controls.clear()
+
+        header = ft.Row(
+            controls=[
+                ft.Text(
+                    "ENKYO",
+                    color=PRIMARY,
+                    size=22,
+                    weight=ft.FontWeight.BOLD,
+                ),
+
+                main_button(
+                    "SAIR",
+                    on_click=home,
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+        )
+
+        ola = ft.Text(
+            "OLÁ, (NOME)!",
+            size=30,
+            weight=ft.FontWeight.BOLD,
+            color=ACCENT,
+            text_align=ft.TextAlign.CENTER,
+        )
+
+        pontos = ft.Text(
+            "Sua pontuação mensal é de\nXX pontos!",
+            size=20,
+            color=PRIMARY,
+            text_align=ft.TextAlign.CENTER,
+            weight=ft.FontWeight.BOLD,
+        )
+
+        agenda_titulo = ft.Text(
+            "AGENDA DO DIA",
+            weight=ft.FontWeight.BOLD,
+            color=PRIMARY,
+            size=22,
+            text_align=ft.TextAlign.CENTER,
+        )
+
+        agenda = ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Text(
+                                    "HORA",
+                                    color=ACCENT,
+                                    weight=ft.FontWeight.BOLD,
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=140,
+                            ),
+
+                            ft.Container(
+                                content=ft.Text(
+                                    "ATIVIDADE",
+                                    color=ACCENT,
+                                    weight=ft.FontWeight.BOLD,
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=180,
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Text(
+                                    "08h30-09h30",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=140,
+                            ),
+
+                            ft.Container(
+                                content=ft.Text(
+                                    "Conversação\nJaponês",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=180,
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Text(
+                                    "08h30-09h30",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=140,
+                            ),
+
+                            ft.Container(
+                                content=ft.Text(
+                                    "Conversação\nMandarim",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=180,
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Text(
+                                    "08h30-09h30",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=140,
+                            ),
+
+                            ft.Container(
+                                content=ft.Text(
+                                    "Kenko Taíso",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=180,
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Text(
+                                    "08h30-09h30",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=140,
+                            ),
+
+                            ft.Container(
+                                content=ft.Text(
+                                    "Smartphone",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=180,
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Text(
+                                    "08h30-09h30",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=140,
+                            ),
+
+                            ft.Container(
+                                content=ft.Text(
+                                    "Dança de Salão",
+                                    color="#4e310a",
+                                    text_align=ft.TextAlign.CENTER,
+                                ),
+                                width=180,
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+                ],
+                spacing=16,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+            padding=20,
+            bgcolor=BG,
+            border_radius=15,
+        )
+
+        card = ft.Container(
+            content=ft.Column(
+                controls=[
+                    header,
+                    ola,
+                    pontos,
+                    agenda_titulo,
+                    agenda,
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=16,
+            ),
+            bgcolor="#c4dbc1",
+            border_radius=20,
+            padding=20,
+            width=420,
+        )
+
+        page.add(
+            ft.Row(
+                controls=[card],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+        )
+
+        page.update()
+
+
+
+
+
+
+    def home(e=None):
+        page.controls.clear()
+        page.bgcolor=BG
+        titulo = ft.Container(
+            content=ft.Text(
+                "ENKYO",
+                size=45,
+                weight=ft.FontWeight.BOLD,
+                color=PRIMARY,
+                text_align=ft.TextAlign.CENTER,
+            ),
+            margin=ft.margin.only(bottom=4),
+        )
+
+
+        subtitulo = ft.Text(
+            "Centro de convivência ao idoso!",
+            color=PRIMARY,
+            size=15,
+            text_align=ft.TextAlign.CENTER,
+
+        )
+
+        botoes = ft.Column( 
+            controls=[
+                main_button("Entrar", color=ACCENT, on_click=go_login),
+                main_button("Cadastrar", color=ACCENT, on_click=go_register),
+            ],
+            spacing=12,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
+
+
+
+        page.add(
+            ft.Column(titulo),
+            ft.Column(subtitulo),
+            ft.Column(botoes),
+        )
+
+        page.update()
+
+    home()
+
+ft.app(target=main)
